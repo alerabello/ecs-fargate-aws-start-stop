b8f5a79802b2257bb3dbf2f27ef1a961a015b461
alexandre.rabello <alerabello95@hotmail.com>
2023-06-01 13:32:16 -0300

    update

--- ecs_fargate_scheduler_lambda.py
+++ ecs_fargate_scheduler_lambda.py
@@ -1,80 +1,49 @@
 import boto3
-import time
-from datetime import datetime, timedelta
-
-# Define Fargate client connection
-ecs = boto3.client('ecs')
-
-DAYS = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-]
+import datetime
+import pytz
 
 def lambda_handler(event, context):
+    # Configurar o cliente do ECS
+    ecs_client = boto3.client('ecs')
 
-    current_time = datetime.now() - timedelta(hours=3)
-    current_time_local = current_time.strftime("%H:%M")
-    print(f'Current time: {current_time_local}')
-    
-    current_day = current_time.strftime("%A")
-    print(f'Current day: {current_day}')
-    	
-    response = ecs.list_clusters()
-    clusters = response['clusterArns']
-    
-    stop_tasks = []   
-    start_tasks = []
+    # Configurar as informações do cluster ECS
+    cluster_name = 'nome-do-cluster'
+    timezone = 'America/Sao_Paulo'
 
-    for cluster in clusters:
-        response = ecs.list_tasks(cluster=cluster)
-        tasks = response['taskArns']
-        
-        for task in tasks:
-            task_details = ecs.describe_tasks(cluster=cluster, tasks=[task])
-            task_definition = task_details['tasks'][0]['taskDefinitionArn']
-            
-            tags_response = ecs.list_tags_for_resource(resourceArn=task_definition)
-            tags = tags_response['tags']
-            
-            periods = []
-            
-            for key in tags:
-                if key.startswith('Period-'):
-                    periods.append(key.split('-')[1])
-            
-            for period in periods:
-                schedule_start = tags.get(f'ScheduleStart-{period}', '')
-                schedule_stop = tags.get(f'ScheduleStop-{period}', '')
-                
-                if current_day == 'Monday' or current_day == 'Tuesday' or current_day == 'Wednesday' or current_day == 'Thursday' or current_day == 'Friday':
-                    if current_time_local == schedule_start:
-                        start_tasks.append(task)
-                    elif current_time_local == schedule_stop:
-                        stop_tasks.append(task)
-                elif current_day == 'Saturday':
-                    if period == '2' and current_time_local == schedule_start:
-                        start_tasks.append(task)
-                elif current_day == 'Sunday':
-                    if period == '3' and current_time_local == schedule_stop:
-                        stop_tasks.append(task)
-            
-    # Stop all tasks tagged to stop.
-    if len(stop_tasks) > 0:
-        for task in stop_tasks:
-            response = ecs.stop_task(cluster=cluster, task=task)
-            print(f'Stopping task: {task}')
+    # Obter a data e hora atual com base no fuso horário configurado
+    current_time = datetime.datetime.now(pytz.timezone(timezone))
+
+    # Obter o dia da semana atual
+    current_day = current_time.strftime('%A')
+
+    # Obter o período e horários de início/fim com base no dia da semana atual
+    if current_day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']:
+        period = 'Period-1'
+        start_time = datetime.time(6, 0)  # 06:00
+        stop_time = datetime.time(18, 0)  # 18:00
+    elif current_day == 'Saturday':
+        period = 'Period-2'
+        start_time = datetime.time(9, 0)  # 09:00
+        stop_time = None
+    elif current_day == 'Sunday':
+        period = 'Period-3'
+        start_time = None
+        stop_time = datetime.time(2, 0)  # 02:00
     else:
-        print("No tasks to stop.")
-        
-    # Start tasks tagged to start. 
-    if len(start_tasks) > 0:
-        for task in start_tasks:
-            response = ecs.start_task(cluster=cluster, task=task)
-            print(f'Starting task: {task}')
-    else:
-        print("No tasks to start.")
+        # Não há programação para outros dias da semana
+        return
+
+    # Obter os ARNs dos serviços do cluster
+    services = ecs_client.list_services(cluster=cluster_name)['serviceArns']
+
+    # Atualizar a configuração dos serviços com base nos horários programados
+    for service in services:
+        ecs_client.update_service(
+            cluster=cluster_name,
+            service=service,
+            schedulingStrategy='REPLICA',  # Ou 'DAEMON', dependendo da estratégia do serviço
+            desiredCount=0 if stop_time else 1  # Definir 0 para parar ou 1 para iniciar o serviço
+        )
+
+    print(f'Serviços do cluster {cluster_name} atualizados com sucesso para o período {period}')
+
